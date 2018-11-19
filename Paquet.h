e8779e8c3aede733f302d200aee5791bfca2c868
nicolasklz <nicolaskleinhentz@gmail.com>
2018-11-19 11:04:32 +0100

    Ajout des fichiers de base

---
+++ Paquet.h
@@ -0,0 +1,17 @@
+#ifndef _PAQUET_H_
+#define _PAQUET_H_
+
+#include <iostream>
+#include <string>
+
+class Paquet {
+
+public:
+  Paquet(); //constructeur
+
+private:
+  //champs privés
+
+}
+
+#endif
